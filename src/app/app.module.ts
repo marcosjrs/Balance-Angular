327d5c9d71112e28b9f2efac7778da61026f4698
Marcos JRS <marcosjrs80@gmail.com>
2020-07-22 10:54:17 +0200

    LLevado html a nuevos componentes: Cabecera, Ingreso, Retirada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { RetiradaComponent } from './retirada/retirada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    IngresoComponent,
+    RetiradaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/retirada/retirada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retirada',
+  templateUrl: './retirada.component.html',
+  styleUrls: ['./retirada.component.css']
+})
+export class RetiradaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
