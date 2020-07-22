3d66c3c628cc2f6a7552ee0032c0d80037df3b6d
Marcos JRS <marcosjrs80@gmail.com>
2020-07-22 11:02:38 +0200

    Refactorizacion moviendo a carpeta componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CabeceraComponent } from './cabecera/cabecera.component';
-import { IngresoComponent } from './ingreso/ingreso.component';
-import { RetiradaComponent } from './retirada/retirada.component';
-import { FormularioMovimientoComponent } from './formulario-movimiento/formulario-movimiento.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { IngresoComponent } from './componentes/ingreso/ingreso.component';
+import { RetiradaComponent } from './componentes/retirada/retirada.component';
+import { FormularioMovimientoComponent } from './componentes/formulario-movimiento/formulario-movimiento.component';
 
 @NgModule({
   declarations: [
